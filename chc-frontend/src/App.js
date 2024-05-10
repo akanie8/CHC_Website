104b5e3df2a5fcafb1088c134a307db493b2313d
Shaun Zitha <docsean2001@gmail.com>
2024-05-10 14:33:42 +0100

    done

--- chc-frontend/src/App.js
+++ chc-frontend/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Switch } from "react-router-dom";
+import {Home} from './screens';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+      <Router>
+
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+
+      </Router>
   );
 }
 
